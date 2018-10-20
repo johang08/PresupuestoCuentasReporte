648633b8d794dd93da4a2aee68f51ff88b1c76b9
johang08 <42912561+johang08@users.noreply.github.com>
2018-10-20 16:13:36 -0400

    Add files via upload

---
+++ PresupuestoCuentas/PatronRepositorioTests/BLL/RepositorioBaseTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PresupuestoCuentas.BLL;
+using PresupuestoCuentas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresupuestoCuentas.BLL.Tests
+{
+    [TestClass()]
+    public class RepositorioBaseTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Cuenta cuenta = new Cuenta();
+            cuenta.CuentaId = 01;
+            cuenta.Descripcion = "Johan";
+            RepositorioBase<Cuenta> repositorio;
+            repositorio = new RepositorioBase<Cuenta>();
+            Assert.IsTrue(repositorio.Guardar(cuenta));
+        }
+    }
+}
